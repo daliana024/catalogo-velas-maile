a631453af492673e47e35df127da7641e4ad92e7
daliana024 <dcarretero@unal.edu.co>
2026-09-05 20:26:56 -0500

    jeje

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,44 @@
-const SUPABASE_URL = "https://wheowewslcmhbqchzjik.supabase.co";
+/* =====================================
+   CONFIGURACIÓN DE SUPABASE
+===================================== */
+
+
+/*
+   URL pública de tu proyecto
+*/
+
+const SUPABASE_URL =
+    "https://wheowewslcmhbqchzjik.supabase.co";
+
+
+/*
+   Publishable Key.
+
+   Esta clave puede utilizarse
+   desde el navegador porque
+   la seguridad real depende
+   de las políticas RLS.
+*/
 
 const SUPABASE_KEY =
     "sb_publishable_9TnYfmwKv7Uxr-hIwSh0lw_cdbBOrrE";
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+
+/* =====================================
+   CREAR CLIENTE
+===================================== */
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
+
+
+/* =====================================
+   COMPROBACIÓN
+===================================== */
+
+console.log(
+    "Supabase conectado correctamente"
 );
